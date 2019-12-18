f9011bd7d23a48120ce2c925b9a8b468300a5620
Truong Pham <truong.pham@logigear.com>
2019-12-18 10:12:44 +0700

    Merged PR 1959: Support mobile

--- java/selenium-core/src/main/java/com/logigear/driver/DriverType.java
+++ java/selenium-core/src/main/java/com/logigear/driver/DriverType.java
@@ -3,9 +3,9 @@ package com.logigear.driver;
 import java.util.Arrays;
 
 public enum DriverType {
-	Chrome, Firefox, IE, Edge, Safari;
+    Chrome, Firefox, IE, Edge, Safari, Android, IOS;
 
-	public static String asString() {
-		return Arrays.toString(DriverType.values()).replaceAll("^.|.$", "");
-	}
+    public static String asString() {
+        return Arrays.toString(DriverType.values()).replaceAll("^.|.$", "");
+    }
 }
