562a3361c4a07afc7ccc3cd856eafa6fdaebe8b6
truong.pham <truong.pham@logigear.com>
2019-12-18 10:04:31 +0700

    Support android and ios driver

---
+++ java/selenium-core/src/main/java/com/logigear/appium/ios/RemoteIOSDriver.java
@@ -0,0 +1,18 @@
+package com.logigear.appium.ios;
+
+import com.logigear.driver.DriverProperty;
+import com.logigear.driver.manager.BaseDriver;
+
+import io.appium.java_client.ios.IOSDriver;
+
+public class RemoteIOSDriver extends BaseDriver {
+
+    public RemoteIOSDriver(DriverProperty property) {
+        super(property);
+    }
+
+    @Override
+    public void createWebDriver() {
+        this.webDriver = new IOSDriver<>(property.getRemoteUrl(), property.getCapabilities());
+    }
+}
