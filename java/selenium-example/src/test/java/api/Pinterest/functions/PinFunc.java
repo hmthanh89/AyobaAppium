f516eb93cf549a0927a9553941534ad79b01333b
VoPhi <phivo.lgg@mail.com>
2019-12-31 13:40:03 +0700

    Update API test cases

--- java/selenium-example/src/test/java/api/Pinterest/functions/PinFunc.java
+++ java/selenium-example/src/test/java/api/Pinterest/functions/PinFunc.java
@@ -1,5 +1,7 @@
 package api.Pinterest.functions;
 
+import java.util.HashMap;
+
 import api.Pinterest.resources.PinApi;
 import api_object.ResponseMessage;
 import data.Pinterest.Board;
@@ -15,14 +17,21 @@ public class PinFunc {
 		api = new PinApi();
 	}
 
-	public String createPin(Board board, Pin pin) {
-		String sboard = Constants.USERNAME + "/" + board.getBoardName();
+	public HashMap<String, String> createPin(Board board, Pin pin) {
+		HashMap<String, String> responseInfo = new HashMap<String, String>();
+		String sboard = Constants.USERNAME + "/" + board.getBoardName().toLowerCase().replaceAll(" ", "-");
 		String body = "{\"board\": \"%s\",\"note\": \"%s\",\"image_url\":\"%s\"}";
 		body = String.format(body, sboard, pin.getNote(), pin.getImageUrl());
 
 		ResponseMessage response = api.createPin(body);
+		if (response==null) {
+			response = api.createPin(body);
+		}
 
-		return response.getOriginalContent(Response.class).getBody().jsonPath().get("message");
+		responseInfo.put("message", response.getOriginalContent(Response.class).getBody().jsonPath().get("message"));
+		responseInfo.put("id", response.getOriginalContent(Response.class).getBody().jsonPath().get("id"));
+		responseInfo.put("statusCode", "" + response.getStatusCode());
+		return responseInfo;
 	}
 
 	public String createPin(Pin pin) {
@@ -30,6 +39,9 @@ public class PinFunc {
 		body = String.format(body, pin.getNote(), pin.getImageUrl());
 
 		ResponseMessage response = api.createPin(body);
+		if (response==null) {
+			response = api.createPin(body);
+		}
 
 		return response.getOriginalContent(Response.class).getBody().jsonPath().get("message");
 	}
--- java/selenium-example/src/test/java/tests/api/PinsTest.java
+++ java/selenium-example/src/test/java/tests/api/PinsTest.java
@@ -1,5 +1,7 @@
 package tests.api;
 
+import java.util.HashMap;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -29,33 +31,35 @@ public class PinsTest {
 	public void TC02_Create_Pin_With_Deleted_Board() {
 		Board board = new Board();
 		Pin pin = new Pin();
+		HashMap<String, String> responseInfo = new HashMap<String, String>();
 		
-		//Logger.info("Create board");
+		Logger.info("Create board");
 		apiBoard.createBoard(board);
 		
-		//Logger.info("Delete created board");
+		Logger.info("Delete created board");
 		apiBoard.deleteBoard(board.getBoardName());
 		
-		//Logger.info("Create pin with deleted board name");
-		String message = apiPin.createPin(board, pin);
+		Logger.info("Create pin with deleted board name");
+		responseInfo = apiPin.createPin(board, pin);
 		
-		//Logger.verify("Verify the error message in response");
-		Assert.assertEquals(message, "Board not found.");
+		Logger.verify("Verify the error message in response");
+		Assert.assertEquals(responseInfo.get("message"), "Board not found.");
 	}
 	
 	@Test(description = "Create pin without optional parameter (link or image or image_url). Verify the response is success")
 	public void TC03_Create_Pin_Without_Optional_Parameter() {
 		Board board = new Board();
 		Pin pin = new Pin();
+		HashMap<String, String> responseInfo = new HashMap<String, String>();
 		
 		Logger.info("Create board");
 		apiBoard.createBoard(board);
 		
 		Logger.info("Create pin without optional parameter");
-		String message = apiPin.createPin(board, pin);
+		responseInfo = apiPin.createPin(board, pin);
 		
 		Logger.verify("Verify the response is success");
-		Assert.assertEquals(message, "");
+		Assert.assertEquals(responseInfo.get("statusCode"), "201");
 	}
 	
 }
